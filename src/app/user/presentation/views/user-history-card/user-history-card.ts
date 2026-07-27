fdbd16f15a26b89b14d69f6905e4d58cc47639a0
samuelbonifacio015 <samuelbonifacio015@gmail.com>
2026-07-27 11:56:47 -0500

    feat(user): show Travel History as a 3rd section in user-history-card

--- src/app/user/presentation/views/user-history-card/user-history-card.ts
+++ src/app/user/presentation/views/user-history-card/user-history-card.ts
@@ -6,6 +6,8 @@ import { MatIcon } from '@angular/material/icon';
 import { CurrentUserViewService } from '../../../../profile/application/current-user-view.service';
 import { TripsApiEndpoint } from '../../../../trip/infrastructure/trips-api-endpoint';
 import { BookingsApiEndpoint } from '../../../../booking/infraestructure/bookings-api-endpoint';
+import { TravelHistoryApiEndpoint } from '../../../../trip/infrastructure/travel-history-api-endpoint';
+import { TravelHistoryEntry } from '../../../../trip/domain/model/travel-history.entity';
 import { UserSettingsStateService } from '../../../application/user-settings-state.service';
 
 @Component({
@@ -19,20 +21,24 @@ export class UserHistoryCard implements OnInit {
   private readonly currentUserView = inject(CurrentUserViewService);
   private readonly tripsApi = inject(TripsApiEndpoint);
   private readonly bookingsApi = inject(BookingsApiEndpoint);
+  private readonly travelHistoryApi = inject(TravelHistoryApiEndpoint);
   private readonly stateService = inject(UserSettingsStateService);
 
   user$ = this.currentUserView.getCurrentUser$();
   trips$: Observable<any[]> = new Observable(observer => observer.next([]));
   bookings$: Observable<any[]> = new Observable(observer => observer.next([]));
+  travelHistory$: Observable<TravelHistoryEntry[]> = new Observable(observer => observer.next([]));
 
   ngOnInit(): void {
     this.user$.subscribe(user => {
       if (user?.id) {
         this.trips$ = this.tripsApi.getMine();
         this.bookings$ = this.bookingsApi.getByUserId(user.id.toString());
+        this.travelHistory$ = this.travelHistoryApi.getByUserId(user.id.toString());
       } else {
         this.trips$ = new Observable(observer => observer.next([]));
         this.bookings$ = new Observable(observer => observer.next([]));
+        this.travelHistory$ = new Observable(observer => observer.next([]));
       }
     });
   }
@@ -70,4 +76,3 @@ export class UserHistoryCard implements OnInit {
     return colors[status] || '#6b7280';
   }
 }
-
